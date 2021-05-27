bff4c243fde3b4071e5099599ddf1a0da00a5079
Juan Arturo Cruz Cardona <arturocrzc@gmail.com>
2021-05-27 12:51:23 -0500

    Set chat to run with covid dataset

--- chat.py
+++ chat.py
@@ -51,6 +51,7 @@ def predict_class(sentence, model):
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
+    result = ""
     for i in list_of_intents:
         if(i['tag']== tag):
             result = random.choice(i['responses'])
